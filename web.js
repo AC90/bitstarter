43cb4ffc8d2d4003176a1a46ac469f7f095b0814
AC90 <swissalex90@hotmail.com>
2013-07-09 09:33:18 +0000

    test

--- web.js
+++ web.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var fs = require('fs');
+var buffer1 = new Buffer( fs.readFileSync('index.html'), "utf-8" );
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    fs.readFileSync('index.html', function (err, data) {
-      if (err) throw err;
-      console.log(data);
-    });
+   buffer1.toString();
 });
 
 var port = process.env.PORT || 5000;
