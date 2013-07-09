fe6171127e194fcccd72fd3a251091a0e12131b0
AC90 <swissalex90@hotmail.com>
2013-07-09 09:21:42 +0000

    test

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(
-    fs.readFile('/index.html', function (err, data) {
+    fs.readFile('index.html', function (err, data) {
       if (err) throw err;
       console.log(data);
     });
